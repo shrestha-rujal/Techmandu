f63f5bb4e7277fda844f591e54bfc7af070a5030
Rujal Shrestha <rujal4shrz@gmail.com>
2020-02-09 09:01:35 +0545

    Database connection setup

--- config/database.php
+++ config/database.php
@@ -1,4 +1,5 @@
 <?php
+$DATABASE_URL=parse_url("postgres://mzfmwwtifinuvm:63862857732c399e8e48e77464f8aef0dc4e0e3253c98d2422faf91dbe47dcca@ec2-184-72-235-80.compute-1.amazonaws.com:5432/da3dti9vgkd78p");
 
 use Illuminate\Support\Str;
 
@@ -15,7 +16,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -65,12 +66,11 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => $DATABASE_URL["host"],
+            'port' => $DATABASE_URL["port"],
+            'database' => ltrim($DATABASE_URL["path"], "/"),
+            'username' => $DATABASE_URL["user"],
+            'password' => $DATABASE_URL["pass"],
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
